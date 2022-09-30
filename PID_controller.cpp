f7529ce11cc19e23dbb40661d46b463b734e59ff
Lukas <lukas_m@gmx.de>
2022-09-30 22:07:46 +0200

    Major rework!
    - i-term can now be tuned on the fly
    - add option to use derivative on measurement to get rid of derivative kick
    - simplified using a defined sample time
    - remove min_mv_tol and min_mv since they were not used anyway

--- PID_controller.cpp
+++ PID_controller.cpp
@@ -1,190 +1,165 @@
-/*
-* PID_controller.cpp
-*
-* Created: 06.01.2017 11:01:56
-* Author: Lukas
-*/
-
-
 #include "PID_controller.h"
-#include "ema_filter.h"
+#include <ema_filter.h>
 
-// constructor
-PID_controller::PID_controller(float K_p_new, float K_i_new, float K_d_new, float min_mv_tol_new, float min_mv_new, float max_mv_new, float max_iterm_new, float ema_filter_p_new, float ema_filter_d_new) {
+#include <Arduino.h>
+
+PID_controller::PID_controller(float K_p_new, float K_i_new, float K_d_new, float max_mv_new, float max_iTerm_new,
+							   float ema_filter_p_new, float ema_filter_d_new, bool derivative_on_measurement_new, float sample_time_new)
+{
 	K_p = K_p_new;
 	K_i = K_i_new;
 	K_d = K_d_new;
-	
-	min_mv_tol = abs(min_mv_tol_new);
-	min_mv = abs(min_mv_new);
+
 	max_mv = abs(max_mv_new);
-	max_iterm = abs(max_iterm_new);
+	max_iTerm = abs(max_iTerm_new);
+
 	ema_filter_p = abs(ema_filter_p_new);
 	ema_filter_d = abs(ema_filter_d_new);
-	
-	update_time_ratio = 1;
-	pid_update_counter = 0;
-	
-	dT_pid = 0;
-	
-	mv = 0;
-	
-	error = 0;
-	error_old = 0;
-	error_d = 0;
-	error_d_old = 0;
-	proportional = 0;
-	integral = 0;
-	integral_old = 0;
-	iterm_old = 0;
-	derivative = 0;
+
+	derivative_on_measurement = derivative_on_measurement_new;
+
+	sample_time = sample_time_new; // use dT as sample time if sample_time is smaller than dT
+
+	reset();
 }
 
-// set targeted PID update time
-void PID_controller::set_pid_update_time(int32_t pid_update_time_new, int32_t update_time_new) {
-	pid_update_time = pid_update_time_new;
-	update_time = update_time_new;
-	
-	update_time_ratio = round((float) pid_update_time / update_time);
-	
-	// update time ratio has to be one at least
-	if (update_time_ratio <= 1) {
-		update_time_ratio = 1;
-	}
-}
-
-// set PID gains
-void PID_controller::set_K_p(float K_p_new) {
+void PID_controller::set_K_p(float K_p_new)
+{
 	K_p = K_p_new;
 }
-void PID_controller::set_K_i(float K_i_new) {
+
+void PID_controller::set_K_i(float K_i_new)
+{
 	K_i = K_i_new;
 }
-void PID_controller::set_K_d(float K_d_new) {
+
+void PID_controller::set_K_d(float K_d_new)
+{
 	K_d = K_d_new;
 }
 
-// set tolerance before minimum min_mv is applied
-void PID_controller::set_min_mv_tol(float min_mv_tol_new) {
-	min_mv_tol = abs(min_mv_tol_new);
-}
-// set absolute minimum for manipulated variable (mv)
-void PID_controller::set_min_mv(float min_mv_new) {
-	min_mv = abs(min_mv_new);
-}
-// set absolute maximum for manipulated variable (mv)
-void PID_controller::set_max_mv(float max_mv_new) {
+void PID_controller::set_max_mv(float max_mv_new)
+{
 	max_mv = abs(max_mv_new);
 }
 
-// set absolute maximum for integral term (iterm)
-void PID_controller::set_max_iterm(float max_iterm_new) {
-	max_iterm = abs(max_iterm_new);
+void PID_controller::set_max_iTerm(float max_iTerm_new)
+{
+	max_iTerm = abs(max_iTerm_new);
 }
 
-// calculate manipulated variable (mv) from setpoint (sp) and process variable (pv)
-float PID_controller::get_mv(float sp, float pv, float dT) {
-	pid_update_counter++;
-	
-	dT_pid += dT;
-	
-	if (pid_update_counter == update_time_ratio) {
-		error = sp - pv;
-		integral += dT_pid * error;
-		
-		iterm = K_i * integral;
-		
-		// limit integral value when integral term or absolute manipulated variable would exceed their limits (wind-up compensation)
-		if ((abs(iterm) >= max_iterm) || ((mv >= max_mv) && (error > 0)) || ((mv <= -max_mv) && (error < 0))) {
-			integral = integral_old;
-			iterm = iterm_old;
-		}
-		
-		if (ema_filter_p < 1) {
-			// filter error for proportional controller
-			proportional = ema_filter(error, proportional, ema_filter_p);
-		}
-		else {
-			proportional = error;
-		}
-		
-		if (ema_filter_d < 1) {
-			// filter error for derivative controller
-			error_d = ema_filter(error, error_d, ema_filter_d);
-		
-			derivative = (error_d - error_d_old) / dT_pid;
-		}
-		else {
-			derivative = (error - error_old) / dT_pid;
-		}
-		
-		mv = K_p * proportional + iterm + K_d * derivative;
-		
-		// constrain manipulated variable (mv)
-		constrain_mv();
-		
-		pid_update_counter = 0;
-		dT_pid = 0;
-		
-		error_old = error;
-		error_d_old = error_d;
-		integral_old = integral;
-		iterm_old = iterm;
+void PID_controller::set_ema_filter_p(float ema_filter_p_new)
+{
+	ema_filter_p = ema_filter_p_new;
+}
+
+void PID_controller::set_ema_filter_d(float ema_filter_p_new)
+{
+	ema_filter_d = ema_filter_p_new;
+}
+
+void PID_controller::set_sample_time(float sample_time_new)
+{
+	sample_time = sample_time_new;
+}
+
+float PID_controller::get_pTerm()
+{
+	return pTerm;
+}
+
+float PID_controller::get_iTerm()
+{
+	return iTerm;
+}
+
+float PID_controller::get_dTerm()
+{
+	return dTerm;
+}
+
+float PID_controller::get_mv(float sp, float pv, float dT)
+{
+	if (dT <= 0)
+	{
+		return mv;
 	}
+
+	dT_pid += dT;
+
+	if (dT_pid >= sample_time)
+	{
+		error = sp - pv;
+
+		// calculate pTerm
+		if (ema_filter_p < 1)
+		{
+			error_filtered = ema_filter(error, error_filtered, ema_filter_p);
+			pTerm = K_p * error_filtered;
+		}
+		else
+		{
+			pTerm = K_p * error;
+		}
+
+		// calculate dTerm
+		if (derivative_on_measurement)
+		{
+			// derivative on measurement to get rid of derivative kick
+			error_d = -pv;
+		}
+		else
+		{
+			error_d = error;
+		}
+
+		if (ema_filter_d < 1)
+		{
+			// filter error_d for derivative controller
+			error_d_filtered = ema_filter(error_d, error_d_filtered, ema_filter_d);
+			dTerm = K_d * (error_d_filtered - error_d_filtered_last) / dT_pid;
+
+			error_d_filtered_last = error_d_filtered;
+		}
+		else
+		{
+			dTerm = K_d * (error_d - error_d_last) / dT_pid;
+
+			error_d_last = error_d;
+		}
+
+		// calculate iTerm
+		iTerm += K_i * dT_pid * error; // multiplying K_i inside the sum allows to adjust K_i on the fly
+
+		// to prevent integral windup, use last iTerm if iterm, or the absolute manipulated variable, would exceed their limits
+		if ((abs(iTerm) > max_iTerm) || ((mv >= max_mv) && (error > 0)) || ((mv <= -max_mv) && (error < 0)))
+		{
+			iTerm = iTerm_last;
+		}
+		iTerm_last = iTerm;
+
+		mv = pTerm + iTerm + dTerm;
+
+		// limit pid output
+		mv = constrain(mv, -max_mv, max_mv);
+
+		dT_pid = 0;
+	}
+
 	return mv;
 }
 
-// get proportional error
-float PID_controller::get_proportional() {
-	return proportional;
-}
-// get integral error
-float PID_controller::get_integral() {
-	return integral;
-}
-// get derivative error
-float PID_controller::get_derivative() {
-	return derivative;
-}
-// get error for derivative controller
-float PID_controller::get_error_d() {
-	return error_d;
-}
-
-// constrain manipulated variable (mv)
-void PID_controller::constrain_mv() {
-	// Increase the manipulated variable |mv| to the minimum (example: the minimum at which the motors start rotating).
-	// If the control variable is within (-min_mv_tol, min_mv_tol), it is set to zero (example: to avoid permanent switching between negative and positive minimum values).
-	if (mv < -min_mv_tol) {
-		mv -= min_mv;
-	}
-	else if (mv > min_mv_tol) {
-		mv += min_mv;
-	}
-	else {
-		mv = 0;
-		return;
-	}
-	
-	// limit manipulated variable (example: maximum motor speed)
-	mv = constrain(mv, -max_mv, max_mv);
-}
-
-// reset PID controller
-void PID_controller::reset() {
-	mv = 0;
-	
-	pid_update_counter = 0;
-	
+void PID_controller::reset()
+{
 	dT_pid = 0;
-	
-	error = 0;
-	error_old = 0;
-	error_d = 0;
-	error_d_old = 0;
-	proportional = 0;
-	integral = 0;
-	integral_old = 0;
-	iterm_old = 0;
-	derivative = 0;
+
+	mv = 0;
+
+	error_filtered = 0;
+	error_d_last = 0;
+	error_d_filtered = 0;
+	error_d_filtered_last = 0;
+
+	iTerm = 0;
+	iTerm_last = 0;
 }
--- PID_controller.h
+++ PID_controller.h
@@ -1,112 +1,61 @@
-/*
-* PID_controller.h
-*
-* Created: 06.01.2017 11:01:56
-* Author: Lukas
-*/
+#ifndef PID_CONTROLLER_H_
+#define PID_CONTROLLER_H_
 
+// ! NO MORE BACKWARDS COMPATIBILITY WITH BB1 OR QUADCOPTER CODE
+class PID_controller
+{
 
-#ifndef __PID_CONTROLLER_H__
-#define __PID_CONTROLLER_H__
+public:
+	PID_controller(float K_p_new, float K_i_new, float K_d_new, float max_mv_new, float max_iTerm_new,
+				   float ema_filter_p_new = 1, float ema_filter_d_new = 1, bool derivative_on_measurement_new = false, float sample_time_new = 0);
 
-#include "Arduino.h"
+	void set_K_p(float K_p_new); // set proportional gain
+	void set_K_i(float K_i_new); // set integral gain
+	void set_K_d(float K_d_new); // set derivative gain
 
-class PID_controller {
-	private:
-	
-	// constrain manipulated variable (mv)
-	void constrain_mv();
-	
-	// manipulated variable (mv)
-	float mv;
-	
-	// proportional gain
-	float K_p;
-	// integral gain
-	float K_i;
-	// derivative gain
-	float K_d;
-	
-	// tolerance before minimum min_mv is applied
-	float min_mv_tol;
-	// absolute minimum for manipulated variable (mv)
-	float min_mv;
-	// absolute maximum for manipulated variable (mv)
-	float max_mv;
-	// absolute maximum for integral term (iterm)
-	float max_iterm;
-	// ema filter value for proportional controller input
-	float ema_filter_p;
-	// ema filter value for derivative controller input
-	float ema_filter_d;
-	
-	// targeted PID update time in microseconds
-	int32_t pid_update_time;
-	
-	// targeted update time in microseconds
-	int32_t update_time;
-	
-	// PID update time to targeted update time ratio
-	int16_t update_time_ratio;
-	// PID update counter
-	int16_t pid_update_counter;
-	
-	// current PID time in seconds
-	float dT_pid;
-	
-	// error between setpoint (sp) and process variable (pv)
-	float error, error_old;
-	// error for derivative controller
-	float error_d, error_d_old;
-	
-	// proportional error
-	float proportional;
-	// integral error
-	float integral, integral_old; 
-	float iterm_old;
-	// derivative error
-	float derivative;
-	
-	// iterm = K_i * integral
-	float iterm;
-	
-	
-	public:
-	
-	// constructor
-	PID_controller(float K_p_new, float K_i_new, float K_d_new, float min_mv_tol_new, float min_mv_new, float max_mv_new, float max_iterm_new, float ema_filter_p_new = 1, float ema_filter_d_new = 1);
-	
-	// set targeted PID update time
-	void set_pid_update_time(int32_t pid_update_time_new, int32_t update_time_new);
-	
-	// set PID gains
-	void set_K_p(float K_p_new);
-	void set_K_i(float K_i_new);
-	void set_K_d(float K_d_new);
-	
-	// set tolerance before minimum min_mv is applied
-	void set_min_mv_tol(float min_mv_tol_new);
-	// set absolute minimum for manipulated variable (mv)
-	void set_min_mv(float min_mv_new);
-	// set absolute maximum for manipulated variable (mv)
-	void set_max_mv(float max_mv_new);
-	// set absolute maximum for integral term (iterm)
-	void set_max_iterm(float max_iterm_new);
-	
-	// calculate manipulated variable (mv) from setpoint (sp) and process variable (pv)
+	void set_max_mv(float max_mv_new);		 // set absolute maximum for manipulated variable
+	void set_max_iTerm(float max_iTerm_new); // set absolute maximum for integral term
+
+	void set_ema_filter_p(float ema_filter_p_new); // set ema-value for proportional error filter
+	void set_ema_filter_d(float ema_filter_d_new); // set ema-value for derivative error filter
+
+	void set_sample_time(float sample_time_new); // set targeted pid-sample time in seconds
+
+	float get_pTerm(); // get proportional term
+	float get_iTerm(); // get integral term
+	float get_dTerm(); // get derivative
+
+	// calculate manipulated variable (mv) from setpoint (sp) and process variable (pv) using the time difference (dT) in seconds
 	float get_mv(float sp_new, float pv_new, float dT);
-	
-	// get proportional error
-	float get_proportional();
-	// get integral error
-	float get_integral();
-	// get derivative error
-	float get_derivative();
-	// get error for derivative controller
-	float get_error_d();
-	
-	// reset PID controller
-	void reset();
+
+	void reset(); // reset PID controller
+
+private:
+	float K_p; // proportional gain
+	float K_i; // integral gain
+	float K_d; // derivative gain
+
+	float max_mv;	 // absolute maximum for manipulated variable (mv)
+	float max_iTerm; // absolute maximum for integral term (iTerm)
+
+	float ema_filter_p; // ema filter value for proportional error
+	float ema_filter_d; // ema filter value for derivative error
+
+	bool derivative_on_measurement; // calculate dTerm using derivative on measurement
+
+	float sample_time; // targeted pid sample time (same unit as dT!)
+	float dT_pid;	   // current pid time
+
+	float mv; // manipulated variable (mv)
+
+	float error;								   // error
+	float error_filtered;						   // filtered error
+	float error_d, error_d_last;				   // error used for derivative term
+	float error_d_filtered, error_d_filtered_last; // filtered error used for derivative term
+
+	float pTerm;			 // proportional term
+	float iTerm, iTerm_last; // integral term
+	float dTerm;			 // derivative term
 };
 
-#endif //__PID_CONTROLLER_H__
+#endif
